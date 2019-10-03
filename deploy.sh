a6153ac3892569f2b857c116cbc82e777974a7b6
Jakub Wolański <jakub.w@mobile-5.com>
2019-10-03 22:49:54 +0200

    fix basedir location

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 docker stop mqttsender
 docker rm mqttsender
-docker run -d -p 8190:80 --name mqttsender -v .:/src mqttsender:latest
+
+BASEDIR=$(pwd)
+docker run -d -p 8190:80 --name mqttsender -v $BASEDIR:/src mqttsender:latest
